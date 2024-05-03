e2e2e17558c8fbc206f8e609ef0250f349ed0b10
Marta Ibarra <martaa.ibarra@hotmail.com>
2024-05-03 15:19:40 +0200

    Add header styles

---
+++ src/components/Header/HeaderStyled.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const HeaderStyled = styled.header`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+
+  .header-logo {
+  }
+`;
+
+export default HeaderStyled;
