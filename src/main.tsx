7677cfe36e6ca2298952acca5324ec7bd8d3697c
Marta Ibarra <martaa.ibarra@hotmail.com>
2024-05-03 12:15:32 +0200

    Add mainTheme

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "@fontsource/space-grotesk";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
---
+++ src/styles/mainTheme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "styled-components";
+
+const mainTheme: DefaultTheme = {
+  colors: {
+    mainLight: "#ffffff",
+    accent: "#6dacbf",
+  },
+  typography: {
+    mainFontFamily: "'space-grotesk'",
+  },
+};
+
+export default mainTheme;
