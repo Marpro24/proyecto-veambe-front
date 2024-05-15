affab0f8baef0b1738c59ab5476310f92800b4f9
Marta Ibarra <martaa.ibarra@hotmail.com>
2024-05-15 15:25:55 +0200

    Add hero to homepage

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Header from "../Header/Header";
 import AppStyled from "./AppStyled";
 import Footer from "../Footer/Footer";
+import Homepage from "../pages/Homepage";
 
 const App = (): React.ReactElement => {
   return (
@@ -9,6 +10,7 @@ const App = (): React.ReactElement => {
       <Header />
       <Routes>
         <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Homepage />} />
         <Route path="/" element={<Navigate to="/portfolio" />} />
         <Route path="/" element={<Navigate to="/sobre-mi" />} />
         <Route path="/" element={<Navigate to="/contacto" />} />
