cb77ad6d22e4d0a72eb5dbda1c3d59a6a1e35621
Marta Ibarra <martaa.ibarra@hotmail.com>
2024-05-15 13:03:57 +0200

    Add footer styles

---
+++ src/components/Footer/FooterStyled.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+const FooterStyled = styled.div`
+  background-color: ${({ theme }) => theme.colors.mainLight};
+  font-size: 1.063rem;
+  bottom: 0;
+  width: 100%;
+  height: 85px;
+  display: flex;
+  justify-content: space-around;
+  position: absolute;
+
+  .footer__text {
+    color: ${({ theme }) => theme.colors.accent};
+    font-size: 1rem;
+    font-family: ${({ theme }) => theme.typography.mainFontFamily};
+  }
+
+  .media__icons {
+    display: flex;
+    gap: 10px;
+    font-size: 2rem;
+  }
+`;
+
+export default FooterStyled;
