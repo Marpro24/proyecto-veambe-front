84bf1db6c318be57e3402ee4667a25bd839d3f7c
Marta Ibarra <martaa.ibarra@hotmail.com>
2024-07-31 21:54:32 +0200

    Add mocked api

---
+++ src/data/VeambesApiMock.ts
@@ -0,0 +1,87 @@
+import { VeambeStructure } from "../store/features/veambe/types";
+
+const veambesApiMock: VeambeStructure[] = [
+  {
+    _id: "1",
+    category: "Mural",
+    title: "(RE)EVOLUCIÓN NATURAL",
+    description:
+      "Intervención en la Galería Urbana de Salamanca organizado por la asociación ZOES.",
+    date: "Mayo 2022",
+    location: "Salamanca",
+    image: "https://i.ibb.co/fHg39hF/img-seccion-mural.png",
+  },
+
+  {
+    _id: "2",
+    category: "Ceramica",
+    title: "(RE)EVOLUCIÓN NATURAL",
+    description:
+      "Intervención en la Galería Urbana de Salamanca organizado por la asociación ZOES.",
+    date: "Mayo 2022",
+    location: "Salamanca",
+    image: "https://i.ibb.co/55Wwvry/img-seccion-ceramica.png",
+  },
+  {
+    _id: "3",
+    category: "Grabado",
+    title: "(RE)EVOLUCIÓN NATURAL",
+    description:
+      "Intervención en la Galería Urbana de Salamanca organizado por la asociación ZOES.",
+    date: "Mayo 2022",
+    location: "Salamanca",
+    image: "https://i.ibb.co/RCf4GT6/img-seccion-grabado.png",
+  },
+  {
+    _id: "4",
+    category: "Ilustración Publicitaria",
+    title: "(RE)EVOLUCIÓN NATURAL",
+    description:
+      "Intervención en la Galería Urbana de Salamanca organizado por la asociación ZOES.",
+    date: "Mayo 2022",
+    location: "Salamanca",
+    image: "https://i.ibb.co/k8BxWdT/img-seccion-ilu-publi.png",
+  },
+  {
+    _id: "5",
+    category: "Textil",
+    title: "(RE)EVOLUCIÓN NATURAL",
+    description:
+      "Intervención en la Galería Urbana de Salamanca organizado por la asociación ZOES.",
+    date: "Mayo 2022",
+    location: "Salamanca",
+    image: "https://i.ibb.co/mD7yNWR/img-seccion-textil.png",
+  },
+  {
+    _id: "6",
+    category: "Autoedición",
+    title: "(RE)EVOLUCIÓN NATURAL",
+    description:
+      "Intervención en la Galería Urbana de Salamanca organizado por la asociación ZOES.",
+    date: "Mayo 2022",
+    location: "Salamanca",
+    image: "https://i.ibb.co/PhNvcw1/img-seccion-autoedicion.png",
+  },
+  {
+    _id: "7",
+    category: "Productos Ilustrados",
+    title: "(RE)EVOLUCIÓN NATURAL",
+    description:
+      "Intervención en la Galería Urbana de Salamanca organizado por la asociación ZOES.",
+    date: "Mayo 2022",
+    location: "Salamanca",
+    image: "https://i.ibb.co/PxYtgWh/img-seccion-productos-ilustrados.png",
+  },
+  {
+    _id: "8",
+    category: "Obra Personal",
+    title: "(RE)EVOLUCIÓN NATURAL",
+    description:
+      "Intervención en la Galería Urbana de Salamanca organizado por la asociación ZOES.",
+    date: "Mayo 2022",
+    location: "Salamanca",
+    image: "https://i.ibb.co/dkwpjMJ/img-seccion-obra-personal.png",
+  },
+];
+
+export default veambesApiMock;
