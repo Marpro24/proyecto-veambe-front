175aa89f470822137a901e5f0ba07bee38ddacd6
Marta Ibarra <martaa.ibarra@hotmail.com>
2024-07-31 21:58:33 +0200

    Add store & provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Provider } from "react-redux";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "@fontsource/space-grotesk";
@@ -6,13 +7,16 @@ import { ThemeProvider } from "styled-components";
 import mainTheme from "./styles/mainTheme";
 import GlobalStyle from "./styles/GlobalStyle";
 import App from "./components/App/App";
+import { store } from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider theme={mainTheme}>
-        <GlobalStyle />
-        <App />
+        <Provider store={store}>
+          <GlobalStyle />
+          <App />
+        </Provider>
       </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
