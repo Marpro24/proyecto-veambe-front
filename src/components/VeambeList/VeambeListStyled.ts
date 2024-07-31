f32fcfb74ecc5cd7579700e0c9360a92910c0be4
Marta Ibarra <martaa.ibarra@hotmail.com>
2024-07-31 21:55:05 +0200

    Add card to list

--- src/components/VeambeList/VeambeListStyled.ts
+++ src/components/VeambeList/VeambeListStyled.ts
@@ -1,7 +1,11 @@
 import styled from "styled-components";
 
-const VeambreListStyled = styled.ul`
+const VeambeListStyled = styled.ul`
   display: grid;
+  justify-items: center;
+  grid-template-columns: 1fr 1fr;
+  gap: 10px;
+  padding: 20px;
 `;
 
-export default VeambreListStyled;
+export default VeambeListStyled;
