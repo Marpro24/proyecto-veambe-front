c2e6842276b7c990e651c09b28a955e12eb21121
Marta Ibarra <martaa.ibarra@hotmail.com>
2024-07-31 21:53:49 +0200

    Add VeambeCard styles

---
+++ src/components/VeambeCard/VeambeCardStyled.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const VeambeCardStyled = styled.article`
+  .veambe-card__image {
+    height: 500px;
+    width: 500px;
+  }
+
+  .veambe-card__category {
+    text-align: center;
+    color: white;
+  }
+`;
+
+export default VeambeCardStyled;
